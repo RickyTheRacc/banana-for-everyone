6de8190e550a7978f1df97754a0b81c5be9962fb
Based Ricky <rickytheracc@gmail.com>
2022-05-30 20:27:15 -0500

    Logo Stuff
    Improve hud logo customizablity and remove unused logos

--- src/main/java/bananaplus/modules/hud/HudLogo.java
+++ src/main/java/bananaplus/modules/hud/HudLogo.java
@@ -2,10 +2,7 @@ package bananaplus.modules.hud;
 
 import meteordevelopment.meteorclient.renderer.GL;
 import meteordevelopment.meteorclient.renderer.Renderer2D;
-import meteordevelopment.meteorclient.settings.DoubleSetting;
-import meteordevelopment.meteorclient.settings.EnumSetting;
-import meteordevelopment.meteorclient.settings.Setting;
-import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.hud.HUD;
 import meteordevelopment.meteorclient.systems.hud.HudRenderer;
 import meteordevelopment.meteorclient.systems.hud.modules.HudElement;
@@ -16,29 +13,71 @@ import net.minecraft.util.Identifier;
 import java.util.Calendar;
 
 public class HudLogo extends HudElement {
-    public enum Mode {
-        Event,
-        Basic
+    public enum Logo {
+        Default,
+        British,
+        Festive,
+        Spooky,
+        Mexican,
+        Irish,
+        Patriot,
+        Pride,
+        Swedish
+    }
+
+    public enum Default {
+        Default,
+        Plus2,
+        Plus3,
+        Circle
+    }
+
+    public enum Halloween {
+        Default,
+        Ghosts,
+        Pumpkin
     }
 
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
 
-    private final Setting<Mode> logo = sgGeneral.add(new EnumSetting.Builder<Mode>()
-            .name("Logo")
-            .description("Which logo to use for the hud")
-            .defaultValue(Mode.Event)
+    // General
+    private final Setting<Logo> logo = sgGeneral.add(new EnumSetting.Builder<Logo>()
+            .name("logo-image")
+            .description("Which image to display as the logo.")
+            .defaultValue(Logo.Default)
+            .build()
+    );
+
+    private final Setting<Boolean> events = sgGeneral.add(new BoolSetting.Builder()
+            .name("events")
+            .description("Use seasonal edits of the logo for holidays!")
+            .defaultValue(true)
+            .build()
+    );
+
+    private final Setting<Default> normal = sgGeneral.add(new EnumSetting.Builder<Default>()
+            .name("default-mode")
+            .description("Which image to display as the logo.")
+            .defaultValue(Default.Default)
+            .build()
+    );
+
+    private final Setting<Halloween> halloween = sgGeneral.add(new EnumSetting.Builder<Halloween>()
+            .name("spooky-mode")
+            .description("Which image to display for the spooky month.")
+            .defaultValue(Halloween.Default)
+            .visible (() -> events.get() || logo.get() == Logo.Spooky)
             .build()
     );
 
     private final Setting<Double> scale = sgGeneral.add(new DoubleSetting.Builder()
             .name("scale")
-            .description("Scale of the logo")
-            .defaultValue(3)
+            .description("How large the logo should be.")
+            .defaultValue(2)
             .min(0.1)
-            .sliderMin(0.1)
-            .sliderMax(4)
+            .sliderRange(0,4)
             .build()
     );
 
@@ -48,17 +87,49 @@ public class HudLogo extends HudElement {
     }
 
 
+    // Normal Logos
     private static final Identifier LOGO = new Identifier("textures", "logo.png");
-    private static final Identifier LOGOC = new Identifier("textures", "circle.png");
-    private static final Identifier LOGOCHRIS = new Identifier("textures", "xmas.png");
-    private static final Identifier LOGOHALLO = new Identifier("textures", "ween.png");
-    private static final Identifier SWEDEN = new Identifier("textures", "sweden.png");
-    private static final Identifier PRIDE = new Identifier("textures", "pride.png");
-    private static final Identifier BRI = new Identifier("textures", "bri.png");
-    private static final Identifier MEX = new Identifier("textures", "mex.png");
-    private static final Identifier USA = new Identifier("textures", "usa.png");
-    private static final Identifier PAT = new Identifier("textures", "patrick.png");
+    private static final Identifier LOGO2 = new Identifier("textures", "logo2.png");
+    private static final Identifier LOGO3 = new Identifier("textures", "logo3.png");
+    private static final Identifier CIRCLE = new Identifier("textures", "circle.png");
 
+
+    // Seasonal Logos
+    private static final Identifier BRITISH = new Identifier("textures", "british.png");
+    private static final Identifier CHRISTMAS = new Identifier("textures", "christmas.png");
+    private static final Identifier HALLOWEEN = new Identifier("textures", "halloween1.png");
+    private static final Identifier HALLOWEEN2 = new Identifier("textures", "halloween2.png");
+    private static final Identifier HALLOWEEN3 = new Identifier("textures", "halloween3.png");
+    private static final Identifier MEXICO = new Identifier("textures", "mexican.png");
+    private static final Identifier PATRICK = new Identifier("textures", "patrick.png");
+    private static final Identifier PATRIOT = new Identifier("textures", "patriot.png");
+    private static final Identifier PRIDE = new Identifier("textures", "pride.png");
+    private static final Identifier SWEDEN = new Identifier("textures", "swedish.png");
+
+    public Identifier getNormal() {
+        if (normal.get() == Default.Default) return LOGO;
+        if (normal.get() == Default.Plus2) return LOGO2;
+        if (normal.get() == Default.Plus3) return LOGO3;
+        return CIRCLE;
+    }
+
+    public Identifier getSpooky() {
+        if (halloween.get() == Halloween.Default) return HALLOWEEN;
+        if (halloween.get() == Halloween.Pumpkin) return HALLOWEEN2;
+        return HALLOWEEN3;
+    }
+
+    public Identifier getDefault() {
+        if (logo.get() == Logo.British) return BRITISH;
+        if (logo.get() == Logo.Festive) return CHRISTMAS;
+        if (logo.get() == Logo.Spooky) return getSpooky();
+        if (logo.get() == Logo.Mexican) return MEXICO;
+        if (logo.get() == Logo.Irish) return PATRICK;
+        if (logo.get() == Logo.Patriot) return PATRIOT;
+        if (logo.get() == Logo.Pride) return PRIDE;
+        if (logo.get() == Logo.Swedish) return SWEDEN;
+        return getNormal();
+    }
 
     @Override
     public void update(HudRenderer renderer) {
@@ -75,21 +146,26 @@ public class HudLogo extends HudElement {
         int h = (int) box.height;
         Color color = new Color (255, 255, 255);
 
-        if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.OCTOBER && logo.get() == Mode.Event) //halloween
-        {
-            GL.bindTexture(LOGOHALLO);
+        if (events.get()) {
+            // June
+            if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.JUNE && Calendar.getInstance().get(Calendar.DATE) == 6) GL.bindTexture(SWEDEN); // Sweden Day
+            else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.JUNE && Calendar.getInstance().get(Calendar.DATE) == 12) GL.bindTexture(BRITISH); // British National Day
+            else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.JUNE) GL.bindTexture(PRIDE); // Pride Month
+            // July
+            else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.JULY && Calendar.getInstance().get(Calendar.DATE) == 4) GL.bindTexture(PATRIOT); // Independence Day
+            // March
+            else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.MARCH && Calendar.getInstance().get(Calendar.DATE) == 17) GL.bindTexture(PATRICK); // St. Patrick's Day
+            // September
+            else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.SEPTEMBER && Calendar.getInstance().get(Calendar.DATE) == 16) GL.bindTexture(MEXICO); // Mexican Independence
+            // October
+            else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.OCTOBER) GL.bindTexture(getSpooky()); // Spooky Month
+            // December
+            else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.DECEMBER && Calendar.getInstance().get(Calendar.DATE) == 25) GL.bindTexture(CHRISTMAS); // Christmas
+            // Default if it's not a holiday
+            else GL.bindTexture(getDefault());
         }
-        else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.DECEMBER && logo.get() == Mode.Event) GL.bindTexture(LOGOCHRIS); //xmas
-        else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.JUNE && Calendar.getInstance().get(Calendar.DATE) == 6 && logo.get() == Mode.Event) GL.bindTexture(SWEDEN); //sweden
-        else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.JUNE && Calendar.getInstance().get(Calendar.DATE) == 12 && logo.get() == Mode.Event) GL.bindTexture(SWEDEN); //brits
-        else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.JUNE && logo.get() == Mode.Event) GL.bindTexture(PRIDE); //pride
-        else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.SEPTEMBER && Calendar.getInstance().get(Calendar.DATE) == 16 && logo.get() == Mode.Event) GL.bindTexture(MEX); //mexico
-        else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.JULY && Calendar.getInstance().get(Calendar.DATE) == 4 && logo.get() == Mode.Event) GL.bindTexture(USA); //usa
-        else if (Calendar.getInstance().get(Calendar.MONTH) == Calendar.MARCH && Calendar.getInstance().get(Calendar.DATE) == 17 && logo.get() == Mode.Event) GL.bindTexture(PAT); //pat
 
-        else if (logo.get() == Mode.Event) GL.bindTexture(LOGO);
-        else if (logo.get() == Mode.Basic) GL.bindTexture(LOGO);
-        else GL.bindTexture(LOGOC);
+        if (!events.get()) GL.bindTexture(getDefault());
 
         Renderer2D.TEXTURE.begin();
         Renderer2D.TEXTURE.texQuad(x, y, w, h, color);
