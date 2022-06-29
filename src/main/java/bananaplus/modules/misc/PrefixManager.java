bc84b545d40675c5b62bdda38d75af5b755d824e
Based Ricky <rickytheracc@gmail.com>
2022-06-28 20:49:20 -0500

    More Prefix Stuffs
    Even more based now I love prefixes

--- src/main/java/bananaplus/modules/misc/PrefixManager.java
+++ src/main/java/bananaplus/modules/misc/PrefixManager.java
@@ -6,12 +6,22 @@ import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 import net.minecraft.text.*;
+import net.minecraft.util.Formatting;
 
 public class PrefixManager extends Module {
     public enum PrefixMode {
         Banana,
         Custom,
         Default
+    }
+
+    public enum Format {
+        Normal,
+        Heavy,
+        Italic,
+        Underline,
+        Crossed,
+        Cursed
     }
 
 
@@ -33,6 +43,23 @@ public class PrefixManager extends Module {
             .description("Color display for the prefix.")
             .defaultValue(new SettingColor(255,193,0,255))
             .onChanged(cope -> setPrefixes())
+            .build()
+    );
+
+    private final Setting<Format> bananaFormatting = sgBanana.add(new EnumSetting.Builder<Format>()
+            .name("prefix-format")
+            .description("What type of minecraft formatting should be applied to the prefix.")
+            .defaultValue(Format.Normal)
+            .onChanged(cope -> setPrefixes())
+            .build()
+    );
+
+    private final Setting<Boolean> bananaFormatBrackets = sgBanana.add(new BoolSetting.Builder()
+            .name("format-brackets")
+            .description("Whether the formatting should apply to the brackets as well.")
+            .visible(() -> bananaFormatting.get() != Format.Normal)
+            .onChanged(cope -> setPrefixes())
+            .defaultValue(true)
             .build()
     );
 
@@ -90,9 +117,27 @@ public class PrefixManager extends Module {
     private final Setting<SettingColor> meteorColor = sgMeteor.add(new ColorSetting.Builder()
             .name("prefix-color")
             .description("Color display for the meteor prefix")
-            .defaultValue(new SettingColor(255, 75, 75, 255))
+            .defaultValue(new SettingColor(142, 60, 222, 255))
             .visible(()-> prefixMode.get() == PrefixMode.Custom)
             .onChanged(cope -> setPrefixes())
+            .build()
+    );
+
+    private final Setting<Format> meteorFormatting = sgMeteor.add(new EnumSetting.Builder<Format>()
+            .name("prefix-format")
+            .description("What type of minecraft formatting should be applied to the prefix.")
+            .defaultValue(Format.Normal)
+            .visible(()-> prefixMode.get() == PrefixMode.Custom)
+            .onChanged(cope -> setPrefixes())
+            .build()
+    );
+
+    private final Setting<Boolean> meteorFormatBrackets = sgBanana.add(new BoolSetting.Builder()
+            .name("format-brackets")
+            .description("Whether the formatting should apply to the brackets as well.")
+            .visible(() -> prefixMode.get() == PrefixMode.Custom && meteorFormatting.get() != Format.Normal)
+            .onChanged(cope -> setPrefixes())
+            .defaultValue(true)
             .build()
     );
 
@@ -115,7 +160,7 @@ public class PrefixManager extends Module {
     );
 
     private final Setting<SettingColor> meteorLeftColor = sgMeteor.add(new ColorSetting.Builder()
-            .name("left-clor")
+            .name("left-color")
             .description("Color display for the left bracket")
             .defaultValue(new SettingColor(150,150,150,255))
             .visible(()-> prefixMode.get() == PrefixMode.Custom)
@@ -150,13 +195,26 @@ public class PrefixManager extends Module {
     }
 
     public void setPrefixes() {
-        ChatUtils.registerCustomPrefix("bananaplus.modules", this::getBananaPrefix);
+        if (isActive()) {
+            ChatUtils.registerCustomPrefix("bananaplus.modules", this::getBananaPrefix);
 
-        switch (prefixMode.get()) {
-            case Banana -> ChatUtils.registerCustomPrefix("meteordevelopment", this::getBananaPrefix);
-            case Custom -> ChatUtils.registerCustomPrefix("meteordevelopment", this::getMeteorPrefix);
-            case Default -> ChatUtils.unregisterCustomPrefix("meteordevelopment");
+            switch (prefixMode.get()) {
+                case Banana -> ChatUtils.registerCustomPrefix("meteordevelopment", this::getBananaPrefix);
+                case Custom -> ChatUtils.registerCustomPrefix("meteordevelopment", this::getMeteorPrefix);
+                case Default -> ChatUtils.unregisterCustomPrefix("meteordevelopment");
+            }
         }
+    }
+
+    private Formatting getFormat(Format format) {
+        return switch (format) {
+            case Normal -> null;
+            case Heavy -> Formatting.BOLD;
+            case Italic -> Formatting.ITALIC;
+            case Underline -> Formatting.UNDERLINE;
+            case Cursed -> Formatting.OBFUSCATED;
+            case Crossed -> Formatting.STRIKETHROUGH;
+        };
     }
 
     public Text getBananaPrefix() {
@@ -165,7 +223,11 @@ public class PrefixManager extends Module {
         MutableText right = Text.literal(bananaRightBracket.get());
         MutableText prefix = Text.literal("");
 
+        if (bananaFormatting.get() != Format.Normal) logo.setStyle(Style.EMPTY.withFormatting(getFormat(bananaFormatting.get())));
         logo.setStyle(logo.getStyle().withColor(TextColor.fromRgb(bananaColor.get().getPacked())));
+
+        if (bananaFormatting.get() != Format.Normal && bananaFormatBrackets.get()) left.setStyle(Style.EMPTY.withFormatting(getFormat(bananaFormatting.get())));
+        if (bananaFormatting.get() != Format.Normal && bananaFormatBrackets.get()) right.setStyle(Style.EMPTY.withFormatting(getFormat(bananaFormatting.get())));
         left.setStyle(left.getStyle().withColor(TextColor.fromRgb(bananaLeftColor.get().getPacked())));
         right.setStyle(right.getStyle().withColor(TextColor.fromRgb(bananaRightColor.get().getPacked())));
 
@@ -183,7 +245,11 @@ public class PrefixManager extends Module {
         MutableText right = Text.literal(meteorRightBracket.get());
         MutableText prefix = Text.literal("");
 
+        if (meteorFormatting.get() != Format.Normal) logo.setStyle(Style.EMPTY.withFormatting(getFormat(meteorFormatting.get())));
         logo.setStyle(logo.getStyle().withColor(TextColor.fromRgb(meteorColor.get().getPacked())));
+
+        if (meteorFormatting.get() != Format.Normal && meteorFormatBrackets.get()) left.setStyle(Style.EMPTY.withFormatting(getFormat(bananaFormatting.get())));
+        if (meteorFormatting.get() != Format.Normal && meteorFormatBrackets.get()) right.setStyle(Style.EMPTY.withFormatting(getFormat(bananaFormatting.get())));
         left.setStyle(left.getStyle().withColor(TextColor.fromRgb(meteorLeftColor.get().getPacked())));
         right.setStyle(right.getStyle().withColor(TextColor.fromRgb(meteorRightColor.get().getPacked())));
 
