48b4208aea6f65f5b5081572c958d7ca2a10089c
Based Ricky <rickytheracc@gmail.com>
2022-07-02 18:35:40 -0500

    Gradel
    Aaaaaaaaaaaaaaaaaaaaa

---
+++ src/main/java/bananaplus/modules/combat/QuiverRewrite.java
@@ -0,0 +1,11 @@
+package bananaplus.modules.combat;
+
+import bananaplus.modules.BananaPlus;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class QuiverRewrite extends Module {
+
+    public QuiverRewrite() {
+        super(BananaPlus.COMBAT, "quiver-plus", "Shoot yourself with ");
+    }
+}
