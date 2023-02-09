455b9e60e0743165336f545d2a554be4b9d381cc
RickyTheRacc <rickytheracc@gmail.com>
2023-02-09 12:39:30 -0600

    Remove Quivers
    Both are obsolete since Meteor's is now AWESOME

--- src/main/java/bananaplus/BananaPlus.java
+++ src/main/java/bananaplus/BananaPlus.java
@@ -67,8 +67,6 @@ public class BananaPlus extends MeteorAddon {
 		Modules.get().add(new MonkeBurrow());
 		Modules.get().add(new Monkhand());
 		Modules.get().add(new MonkeTotem());
-		Modules.get().add(new QuiverPlus());
-		Modules.get().add(new QuiverRewrite());
 		Modules.get().add(new SelfTrapPlus());
 		Modules.get().add(new SmartHoleFill());
 		Modules.get().add(new StepPlus());
